c3409659af7c7bd4fec07f4769ddfd11320d83ec
Karthickramasamy007 <10cse61@gmail.com>
2023-10-09 21:31:52 +0100

    feat: initial commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express Karthick' });
+  res.render('index', { title: 'Express Karthick 1' });
 });
 
 module.exports = router;
